3c7997858c2beeb56c8b31d3aefd9b13747dd1b5
safey11 <135832600+adnanmk4@users.noreply.github.com>
2025-08-15 11:14:13 -0700

    pusssshhhhh

--- tweete/src/components/PostList.jsx
+++ tweete/src/components/PostList.jsx
@@ -3,16 +3,24 @@ import Post from "./Post"
 import classes from './postList.module.css'
 
 const PostList = () => {
+
+  const [enteredBody, setEnteredBody] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
   return (
     <>
-    <NewPost />
-    
+
+    <NewPost onBodyChange={bodyChangeHandler} />    
     <ul className={classes.posts}>
-    <Post author="Master" body="Adnan is awsome" />
+    <Post author="Master" body={enteredBody} />
     <Post author="Saif" body="my master is awsome" />
     </ul>
+
     </>
   )
 }
 
-export default PostList
+export default PostList;
